af8955fd25a4e085cfa5a49c3c34af38cf6b78bd
Enzo Nguyen <enzo.brightsoft@gmail.com>
2025-06-06 22:58:58 +0700

    login register UI

---
+++ SREM.AI.Web/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SREM.AI.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login(string returnUrl = "")
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
